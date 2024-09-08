4420f92da39eb30e2e7fc6556846a2634cf031a1
Farouq Seriki <farouq@basaltx.io>
2024-09-08 21:28:02 +0100

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,22 @@
+import { Rise } from '@rise-tools/react';
 import { Stack, Link } from 'expo-router';
 
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
+import { modelSource } from '~/src/modelSource';
+import { useRiseActions } from '~/src/riseActions';
+import { components } from '~/src/riseComponents';
 
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
+      <Rise
+        modelSource={modelSource}
+        components={components}
+        path="HomeScreen"
+        actions={useRiseActions()}
+      />
     </>
   );
 }
---
+++ src/modelSource.ts
@@ -0,0 +1,3 @@
+import { createWSModelSource } from "@rise-tools/ws-client";
+
+export const modelSource = createWSModelSource("ws://192.168.0.213:3015");
